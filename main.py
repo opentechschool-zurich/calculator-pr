49f1636fced32c5fe6e584bfadfdee2e0ff3e687
louis quatorze <louisquatorze@louiss-MacBook-Pro.local>
2025-11-11 20:07:47 +0100

    things

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ def calculate(a, op, b):
         return a + b
     return None
 
+def calculate_list(list_to_calculate):
+    result = 0
+    if len(list_to_calculate) < 3:
+        return list_to_calculate[1]
+    for u in range(0, len(list_to_calculate)-3, 3):
+        result = calculate(list_to_calculate[u], list_to_calculate[u+1], list_to_calculate[u+2])
+        list_to_calculate[u+2] = result
+    return result
+
 def main():
     assert calculate(3, '+', 2) == 5
 
